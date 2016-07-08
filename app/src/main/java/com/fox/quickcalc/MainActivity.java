1bac6de61af46c80a92c35fd2f9108913f613165
Stephen Fox <stevethecoder34@gmail.com>
2016-07-07 22:51:04 -0600

    Scrapped everything because I want Fragment controlled views. Moving one piece at a time now

--- app/src/main/java/com/fox/quickcalc/MainActivity.java
+++ app/src/main/java/com/fox/quickcalc/MainActivity.java
@@ -1,7 +1,9 @@
 package com.fox.quickcalc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout _frameLayout = (FrameLayout) findViewById(R.id.result_frame_layout);
+        TextView _textView = new TextView(this);
+        _textView.setText("Hello World");
+        _frameLayout.addView(_textView);
+
+        FrameLayout bottomFrame = (FrameLayout) findViewById(R.id.keypad_frame_layout);
+        TextView _otherText = new TextView(this);
+        _otherText.setText("Bottom view, saying 'hey'");
+        bottomFrame.addView(_otherText);
     }
 }
